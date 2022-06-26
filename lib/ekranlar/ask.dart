8efcb93d8adbc258e92eaea8bc11f0f3e94f05fa
tugcegultekin <76662575+tugcegultekin@users.noreply.github.com>
2022-06-26 23:50:04 +0300

    Update ask.dart

--- lib/ekranlar/ask.dart
+++ lib/ekranlar/ask.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -21,27 +20,27 @@ class AskRoman extends State<Ask> {
               ElevatedButton(
                 onPressed: () {
                   Random rnd = new Random();
-                  var rnd2 = rnd.nextInt(75);
+                  var rnd2 = rnd.nextInt(9);
 
-                  if (rnd2 > 1 && rnd2 < 5) {
+                  if (rnd2 > 1 && rnd2 <=3) {
                     var alert = const AlertDialog(
                       title: Text("Uğultulu Tepeler\nEmily Bronte"),
                     );
                     showDialog(
                         context: context, builder: (BuildContext context) => alert);
-                  } else if (rnd2 > 6 && rnd2 < 10) {
+                  } else if (rnd2 > 3 && rnd2 <=5) {
                     var alert = const AlertDialog(
                       title: Text("Gurur ve Önyargı\nYazar: Jane Austen"),
                     );
                     showDialog(
                         context: context, builder: (BuildContext context) => alert);
-                  } else if (rnd2 > 11 && rnd2 < 15) {
+                  } else if (rnd2 > 5 && rnd2 <=7) {
                     var alert = const AlertDialog(
                       title: Text("Vadideki Zambak\nYazar: Honoré de Balzac"),
                     );
                     showDialog(
                         context: context, builder: (BuildContext context) => alert);
-                  } else if (rnd2 > 16 && rnd2 < 20) {
+                  } else if (rnd2 > 7 && rnd2 <=9) {
                     var alert = const AlertDialog(
                       title: Text("Eylül\nYazar: Mehmet Rauf"),
                     );
@@ -56,4 +55,3 @@ class AskRoman extends State<Ask> {
     );
   }
 
-}
